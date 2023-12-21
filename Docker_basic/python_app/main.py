cacec16a972b1d02ae563674b98456a46b9608f6
mmdaya <mmdayashankar@gmail.com>
2023-12-21 13:39:08 +0530

    ds

--- Docker_basic/python_app/main.py
+++ Docker_basic/python_app/main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def myname():
         return "Dayashankar M M"
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", port="8080")
+        app.run(host="0.0.0.0", port="8083")
